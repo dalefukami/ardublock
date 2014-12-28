f22574b5a3b85bf84718ca30846d818092bab666
Dale Fukami <dale.fukami@gmail.com>
2014-12-28 10:14:28 -0700

    Add initial ifSensor1On block
    No back end cpp code yet. Just visual block and code gen.

---
+++ src/main/java/com/ardublock/translator/block/roboblocks/IfSensor1On.java
@@ -0,0 +1,31 @@
+package com.ardublock.translator.block.roboblocks;
+
+import com.ardublock.translator.Translator;
+import com.ardublock.translator.block.TranslatorBlock;
+import com.ardublock.translator.block.exception.SocketNullException;
+import com.ardublock.translator.block.exception.SubroutineNotDeclaredException;
+
+public class IfSensor1On extends TranslatorBlock
+{
+	public IfSensor1On(Long blockId, Translator translator, String codePrefix, String codeSuffix, String label)
+	{
+		super(blockId, translator);
+	}
+
+	@Override
+	public String toCode() throws SocketNullException, SubroutineNotDeclaredException
+	{		
+		RoboblocksUtil.setupEnv(translator);
+	
+		String ret = "if (robo.sensorIsOn(1)) {\n";
+		
+		TranslatorBlock translatorBlock = getTranslatorBlockAtSocket(0);
+		while (translatorBlock != null)
+		{
+			ret = ret + translatorBlock.toCode();
+			translatorBlock = translatorBlock.nextTranslatorBlock();
+		}
+		ret = ret + "}\n";
+		return ret;
+	}
+}
