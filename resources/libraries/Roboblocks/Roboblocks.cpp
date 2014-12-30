133b1c6703317245281caaba23e55a9c1859dad8
Dale Fukami <dale.fukami@gmail.com>
2014-12-30 14:54:03 -0700

    Add cpp library code for sensor and motor control

--- resources/libraries/Roboblocks/Roboblocks.cpp
+++ resources/libraries/Roboblocks/Roboblocks.cpp
@@ -15,6 +15,56 @@ void Roboblocks::blinkLed(void)
   }
 }
 
+bool Roboblocks::sensorIsOn(int sensorNumber) {
+    return digitalRead(sensorPins[sensorNumber - 1]);
+}
 
+bool Roboblocks::sensorIsOff(int sensorNumber) {
+    return !digitalRead(sensorPins[sensorNumber - 1]);
+}
 
+void Roboblocks::goForward(void) {
+    leftMotorForward();
+    rightMotorForward();
+}
 
+void Roboblocks::goBackward(void) {
+    leftMotorBackward();
+    rightMotorBackward();
+}
+
+void Roboblocks::turnRight(void) {
+    leftMotorForward();
+    rightMotorBackward();
+}
+
+void Roboblocks::turnLeft(void) {
+    leftMotorBackward();
+    rightMotorForward();
+}
+
+void Roboblocks::leftMotorForward(void) {
+    motorForward(leftMotorPin1, leftMotorPin2);
+}
+
+void Roboblocks::leftMotorBackward(void) {
+    motorBackward(leftMotorPin1, leftMotorPin2);
+}
+
+void Roboblocks::rightMotorForward(void) {
+    motorForward(rightMotorPin1, rightMotorPin2);
+}
+
+void Roboblocks::rightMotorBackward(void) {
+    motorBackward(rightMotorPin1, rightMotorPin2);
+}
+
+void Roboblocks::motorForward(int pin1, int pin2) {
+    digitalWrite(pin1, LOW);
+    digitalWrite(pin2, HIGH);
+}
+
+void Roboblocks::motorBackward(int pin1, int pin2) {
+    digitalWrite(pin1, HIGH);
+    digitalWrite(pin2, LOW);
+}
--- resources/libraries/Roboblocks/Roboblocks.h
+++ resources/libraries/Roboblocks/Roboblocks.h
@@ -11,10 +11,31 @@
 class Roboblocks
 {
 private:
+  int leftMotorPin1 = 11;
+  int leftMotorPin2 = 12;
+  int rightMotorPin1 = 9;
+  int rightMotorPin2 = 10;
+  int sensorPins[0] = {3,4,5,6,7};
+
+  void leftMotorForward(void);
+  void leftMotorBackward(void);
+  void rightMotorForward(void);
+  void rightMotorBackward(void);
+  void motorForward(int pin1, int pin2);
+  void motorBackward(int pin1, int pin2);
+
 public:
   Roboblocks(void);
 
   void blinkLed(void);
+
+  bool sensorIsOn(int sensorNumber);
+  bool sensorIsOff(int sensorNumber);
+
+  void goForward(void);
+  void goBackward(void);
+  void turnLeft(void);
+  void turnRight(void);
 };
 
 #endif
