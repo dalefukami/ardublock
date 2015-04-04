af4f6a8143aec4bd24499c749575291d33ed1747
Dale Fukami <dale.fukami@gmail.com>
2015-04-04 09:20:37 -0600

    Make work on windows

--- resources/libraries/Roboblocks/Roboblocks.h
+++ resources/libraries/Roboblocks/Roboblocks.h
@@ -9,18 +9,18 @@
 #endif
 
 const int ROBOBLOCKS_SENSOR_COUNT = 5;
+const int speedPin = 9; // PWM pin for speed control
+const int speed = 128;
+const int leftMotorPin1 = 11;
+const int leftMotorPin2 = 12;
+const int rightMotorPin1 = 8;
+const int rightMotorPin2 = 10;
+const int sensorCount = ROBOBLOCKS_SENSOR_COUNT;
+const int sensorPins[ROBOBLOCKS_SENSOR_COUNT] = {3,4,5,6,7};
 
 class Roboblocks
 {
 private:
-  int speedPin = 9; // PWM pin for speed control
-  int speed = 128;
-  int leftMotorPin1 = 11;
-  int leftMotorPin2 = 12;
-  int rightMotorPin1 = 8;
-  int rightMotorPin2 = 10;
-  int sensorCount = ROBOBLOCKS_SENSOR_COUNT;
-  int sensorPins[ROBOBLOCKS_SENSOR_COUNT] = {3,4,5,6,7};
 
   void leftMotorForward(void);
   void leftMotorBackward(void);
